fc08ced5fe0f9ada3727b1718c52de584b0621ed
M. Folkers <hello@twobrain.io>
2017-10-14 01:29:53 +0200

    Updated dependencies & tidied up javascript

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -4,14 +4,51 @@
  * Importing functions ..
  */
 
-import featureDetection from './partials/featureDetection';
-import astroJS from './partials/headerNav';
-import backToTop from './partials/backToTop';
-import macyJS from './partials/macy';
+import Astro from 'Astro';
+import macy from 'macy';
+
 
 /*
  * .. and executing them
  */
+
+function featureDetection() {
+  let className = '';
+  let html = '';
+  html = document.documentElement;
+  className = html.className.replace('no-js', 'js');
+  html.className = className;
+}
+
+function astroJS() {
+  Astro.init({
+    toggleActiveClass: 'is-active',
+    navActiveClass: 'is-active',
+  });
+}
+
+function backToTop() {
+  window.onscroll = function() {
+    const toTop = document.getElementById('js-back-to-top');
+    if (window.pageYOffset > 200) {
+      toTop.classList.add('back-to-top--is-visible');
+    } else {
+      toTop.classList.remove('back-to-top--is-visible');
+    }
+  };
+}
+
+function macyJS() {
+  macy({
+    container: '#macy',
+    trueOrder: false,
+    columns: 2,
+    breakAt: {
+      768: 1,
+    },
+  });
+}
+
 
 featureDetection();
 astroJS();
--- source/scripts/partials/featureDetection.js
+++
@@ -1,8 +0,0 @@
-// Simple JS feature detection
-export default function featureDetection() {
-  let className = '';
-  let html = '';
-  html = document.documentElement;
-  className = html.className.replace('no-js', 'js');
-  html.className = className;
-}
