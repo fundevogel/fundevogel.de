6427154a6cd2ac1dc290e21936fc9160afdbeea8
M. Folkers <hello@twobrain.io>
2017-10-14 01:36:42 +0200

    Cleaned config.php

--- build/site/config/config.php
+++ build/site/config/config.php
@@ -8,7 +8,4 @@ include kirby()->roots()->config() . '/languages.php';
 // Development settings
 
 c::set('debug', true);
-c::set('fingerprint', false);
-c::set('cache', false);
 c::set('thumbs.driver', 'im');
-c::set('plugin.compress', true);
