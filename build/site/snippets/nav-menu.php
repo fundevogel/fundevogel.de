e7711a3625e7714fe52bbd666eea2fe9278739d0
M. Folkers <hello@twobrain.io>
2017-10-17 00:16:15 +0200

    Added menu filter (show only translated pages)

--- build/site/snippets/nav-menu.php
+++ build/site/snippets/nav-menu.php
@@ -1,5 +1,10 @@
 <ul class="inline-list spread-out">
-  <?php foreach($pages->visible()->filterBy('translation_ready', '1') as $item) : ?>
+  <?php
+    $translatedPages = $pages->visible()->filterBy('translation_ready', '1')->filter(function($p) {
+      return $p->content(site()->language()->code())->exists();
+    });
+  ?>
+  <?php foreach($translatedPages as $item) : ?>
     <li>
       <a class="<?= r($item->isOpen(), 'is-active') ?>" href="<?= $item->url() ?>" <?= r($page->isHomePage(), 'rel="home"') ?> title="<?php e($item->is('home'), l::get('startseite'), $item->title()->html()) ?>">
         <?php
