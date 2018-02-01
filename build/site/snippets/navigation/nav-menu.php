34fed655f4b6264262c22168eea81e3842514a2f
M. Folkers <hello@twobrain.io>
2018-02-01 19:39:36 +0100

    Removing lang filter for header-nav (customer)

--- build/site/snippets/navigation/nav-menu.php
+++ build/site/snippets/navigation/nav-menu.php
@@ -1,11 +1,11 @@
 <nav id="nav-menu" class="nav-menu">
   <ul class="inline-list spread-out">
     <?php
-      $translated = $pages->visible()->filterBy('translations', '1')->filter(function($p) {
-        return $p->content(site()->language()->code())->exists();
-      });
+      // $translated = $pages->visible()->filterBy('translations', '1')->filter(function($p) {
+      //   return $p->content(site()->language()->code())->exists();
+      // });
     ?>
-    <?php foreach($translated as $item) : ?>
+    <?php foreach($pages->visible() as $item) : ?>
     <li>
       <a<?php e($item->isOpen(), ' class="is-active"') ?> href="<?= $item->url() ?>" title="<?php e($item->isHomePage(), l::get('startseite'), $item->title()->html()) ?>">
         <?php
