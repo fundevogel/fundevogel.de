4e1d6a55aaa1679e973659c4ae87db1b4d063407
M. Folkers <hello@twobrain.io>
2018-02-04 20:16:39 +0100

    Replacing slick-slider with tiny-slider See #3 (part 1)

--- build/site/controllers/fundevogel.php
+++ build/site/controllers/fundevogel.php
@@ -3,15 +3,9 @@
 return function($site, $pages, $page) {
 
   $images = $page->images();
-  $first = $images->first();
 
-  $options = array(
-    'lazyLoad' => 'ondemand',
-    'autoplay' => true,
-    'arrows' => false,
-    'fade' => true,
-    'cssEase' => 'linear'
+  return compact(
+    'images',
+    'first'
   );
-
-  return compact('images', 'first', 'options');
 };
---
+++ build/site/snippets/cover/gallery.php
@@ -0,0 +1,15 @@
+<figure class="fig fig--gallery has-hover">
+  <div class="gallery">
+    <?php
+      foreach ($images as $image) :
+      $thumb = $image->crop(460, 400, 85);
+    ?>
+    <a href="<?= $image->url() ?>" data-fancybox="fundevogel-und-team" data-caption="<?= $image->caption()->html() ?>">
+      <img src ="<?= $thumb->url() ?>" title="<?= $image->caption()->html() ?>" alt="<?= $image->alt()->html() ?>" width="<?= $thumb->width() ?>" height="<?= $thumb->height() ?>">
+    </a>
+    <?php endforeach ?>
+  </div>
+  <figcaption class="sketch bg--primary">
+    <?= $page->caption()->html() ?>
+  </figcaption>
+</figure>
