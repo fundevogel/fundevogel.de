94f75a13da22da72118c5d3de24af1db91e0b4c0
M. Folkers <hello@twobrain.io>
2018-02-07 01:35:10 +0100

    Adding jQuery to JS workflow Making it work with webpack & moving jQuery-related code

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -4,9 +4,13 @@
  * Importing functions ..
  */
 
+import jQuery from 'jquery';
 import Astro from 'Astro';
 import macy from 'macy';
 import { tns } from 'tiny-slider/src/tiny-slider.module';
+
+window.$ = window.jQuery = jQuery;
+
 
 /*
  * .. and executing them
@@ -36,6 +40,45 @@ function backToTop() {
       toTop.classList.remove('back-to-top--is-visible');
     }
   };
+
+  // SmoothScroll back-to-top on all pages
+
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 }
 
 function macyJS() {
