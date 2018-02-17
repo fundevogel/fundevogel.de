bb954b59ee3917078889671fe437ff66c76ef45d
M. Folkers <hello@twobrain.io>
2018-02-18 00:59:59 +0100

    Fixing $perpage error (undefined)

--- build/site/controllers/home.php
+++ build/site/controllers/home.php
@@ -1,16 +1,17 @@
 <?php
 
-  return function($site, $pages, $page) {
+return function($site, $pages, $page) {
 
-    $all = $page->children()
-                ->visible()
-                ->flip();
-    
-    $posts = $all->paginate(($perpage >= 1) ? $perpage : 5);
+  $perpage = $page->perpage()->int();
+  $all     = $page->children()
+              ->visible()
+              ->flip();
 
-    return [
-      'posts' => $posts,
-      'pagination' => $posts->pagination(),
-      'last' => $all->last()
-    ];
+  $posts = $all->paginate(($perpage >= 1) ? $perpage : 5);
+
+  return [
+    'posts' => $posts,
+    'pagination' => $posts->pagination(),
+    'last' => $all->last()
+  ];
 };
--- build/site/snippets/partials/post.php
+++ build/site/snippets/partials/post.php
@@ -2,25 +2,27 @@
   $image = $post->image();
   $images = $post->images();
 ?>
-<article class="wrap<?php e(r::ajax(), ' ajaxd') ?> post">
-<?php if (count($images) == 1) : ?>
-<div class="one-third image-area lightgallery">
-  <?php snippet('cover/postcover', ['post' => $post, 'image' => $image, 'gallery' => false]) ?>
-</div>
-<div class="two-thirds">
-  <time datetime="<?= $post->date('Y-m-d') ?>"><?= $post->date('d.m.Y') ?></time><span class="subtitle"> - <?= $post->subtitle()->html() ?></span>
-  <h3><?= $post->title()->html() ?></h3>
-  <?= $post->text()->kirbytext() ?>
-</div>
-<?php else : ?>
-<time datetime="<?= $post->date('Y-m-d') ?>"><?= $post->date('d.m.Y') ?></time><span class="subtitle"> - <?= $post->subtitle()->html() ?></span>
-<h3><?= $post->title()->html() ?></h3>
-<?= $post->text()->kirbytext() ?>
-<div class="image-area lightgallery">
-  <?php foreach ($images as $image) : ?>
-    <?php snippet('cover/postcover', ['post' => $post, 'image' => $image, 'gallery' => true]) ?>
-  <?php endforeach ?>
-</div>
-<?php endif ?>
+<article class="post">
+  <div class="wrap">
+    <?php if (count($images) == 1) : ?>
+      <div class="one-third image-area lightgallery">
+        <?php snippet('cover/postcover', ['post' => $post, 'image' => $image, 'gallery' => false]) ?>
+      </div>
+      <div class="two-thirds">
+        <time datetime="<?= $post->date('Y-m-d') ?>"><?= $post->date('d.m.Y') ?></time><span class="subtitle"> - <?= $post->subtitle()->html() ?></span>
+        <h3><?= $post->title()->html() ?></h3>
+        <?= $post->text()->kirbytext() ?>
+      </div>
+    <?php else : ?>
+      <time datetime="<?= $post->date('Y-m-d') ?>"><?= $post->date('d.m.Y') ?></time><span class="subtitle"> - <?= $post->subtitle()->html() ?></span>
+      <h3><?= $post->title()->html() ?></h3>
+      <?= $post->text()->kirbytext() ?>
+      <div class="image-area lightgallery">
+        <?php foreach ($images as $image) : ?>
+          <?php snippet('cover/postcover', ['post' => $post, 'image' => $image, 'gallery' => true]) ?>
+        <?php endforeach ?>
+      </div>
+    <?php endif ?>
+  </div>
 <?php e($post !== $last, '<hr>', '<h3 class="center">' . l::get('home_mehr-anzeigen--ende') . '</h3>') ?>
 </article>
