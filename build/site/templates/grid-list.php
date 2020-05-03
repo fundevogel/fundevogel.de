10efc7afa65e95e37dd68695ff4450851fc15962
Martin Folkers <hello@twobrain.io>
2020-05-03 21:39:36 +0200

    Completing 'grid-list'

--- build/site/templates/grid-list.php
+++ build/site/templates/grid-list.php
@@ -1,37 +1,41 @@
-<?php
-
-use PHPCBIS\PHPCBIS;
-use PHPCBIS\Helpers\Butler;
-
-
- ?>
-
 <?php snippet('header') ?>
 
-<header>
-  <div class="wrap">
-    <div class="one-half--wide">
-      <?= $page->text()->kt() ?>
-    </div>
-    <div class="one-half--wide teaser">
-      <?php if ($page == page('unser-netzwerk')) : ?>
-        <p class="motto large-down">
-          <?= $page->motto()->html() ?>
-        </p>
-      <?php endif ?>
-      <?php snippet('cover') ?>
-    </div>
-  </div>
-  <?php if ($page == page('unser-netzwerk')) : ?>
-  <div class="wrap motto center show-large-up">
-    <?= $page->motto()->kt() ?>
-  </div>
-  <?php endif ?>
-</header>
-<hr>
-<section class="list wrap">
-  <h2><?= t($identifier . '_ueberschrift-liste') ?>:</h2>
-  <?php snippet('templates/grid-list.cards', $cards) ?>
-</section>
+<article class="mb-16">
+    <header class="container">
+        <div class="flex flex-col lg:flex-row">
+            <div class="flex-1">
+                <?= $page->text()->kt() ?>
+            </div>
+            <div class="pt-6 lg:pt-12 flex-none text-center">
+                <?php if ($page == page('unser-netzwerk')) : ?>
+                <p class="max-w-md mb-10 font-bold font-small-caps text-center text-red-medium inline-block lg:hidden">
+                    <?= $page->motto()->html() ?>
+                </p>
+                <?php endif ?>
+                <?php snippet('cover') ?>
+            </div>
+        </div>
+        <?php if ($page == page('unser-netzwerk')) : ?>
+        <div class="mt-12 text-center">
+            <p class="max-w-md font-bold font-small-caps text-red-medium hidden lg:inline-block">
+                <?= $page->motto()->html() ?>
+            </p>
+        </div>
+        <?php endif ?>
+    </header>
+    <hr>
+    <section class="container">
+        <h2 class="mb-12 text-center"><?= t($identifier . '_ueberschrift-liste') ?>:</h2>
+        <div id="macy">
+            <?php foreach($cards as $card) : ?>
+            <article class="card">
+                <div class="card-inner">
+                    <?= $card->entry()->kt() ?>
+                </div>
+            </article>
+            <?php endforeach ?>
+        </div>
+    </section>
+</article>
 
 <?php snippet('footer') ?>
