08641de5b92155571d61daaf25bc6a598814a896
Martin Folkers <hello@twobrain.io>
2020-05-05 22:23:00 +0200

    Completing 'normal' pages

--- build/site/templates/imprint.php
+++ build/site/templates/imprint.php
@@ -1,12 +1,17 @@
 <?php snippet('header') ?>
 
-<section class="wrap">
-  <h2>Fundevogel</h2>
-  <div class="one-half">
-    <?= $page->left()->kt() ?>
-  </div>
-  <div class="one-half">
-    <?= $page->right()->kt() ?>
-  </div>
+<article class="mb-16">
+    <section class="container">
+        <h2>Fundevogel</h2>
+        <div class="flex flex-col lg:flex-row">
+            <div class="flex-1">
+                <?= $page->left()->kt() ?>
+            </div>
+            <div class="flex-1 lg:ml-10">
+                <?= $page->right()->kt() ?>
+            </div>
+        </div>
+    </section>
+</article>
 
 <?php snippet('footer') ?>
