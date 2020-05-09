5705feab1c48a9bd8abe40f42650bc0557646ba9
Martin Folkers <hello@twobrain.io>
2020-05-09 19:03:59 +0200

    Completing 'lesetipp' single view

--- build/site/controllers/lesetipps.article.php
+++ build/site/controllers/lesetipps.article.php
@@ -2,8 +2,16 @@
 
 return function ($site, $page) {
     $cover = $page->cover()->isNotEmpty() ? $page->cover()->toFile() : $site->fallback()->toFile();
+    $thumb = $cover->thumb('lesetipps.article.cover');
+
+    $age_list = explode(' ', $page->alter());
+    $period = array_pop($age_list);
+    $age = implode(' ', $age_list);
 
     return compact(
         'cover',
+        'thumb',
+        'age',
+        'period'
     );
 };
--- build/site/snippets/templates/lesetipps.article.book.php
+++
@@ -1,8 +0,0 @@
-<?= $lesetipp->verdict()->kt() ?>
-<blockquote>
-  <?= $lesetipp->titel()->html() ?><br>
-  <?= $lesetipp->autor()->html() ?><br>
-  <?= $lesetipp->verlag()->html() ?><br>
-  ISBN <?= $lesetipp->isbn()->html() ?><br>
-  <?= $lesetipp->preis()->html() ?> €<?php e($lesetipp->alter()->isNotEmpty(), '; ' . $lesetipp->alter()->html()) ?>
-</blockquote>
