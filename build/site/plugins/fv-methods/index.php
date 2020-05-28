e3659fde5041e1b705e912f9d09c9017c299f27a
Martin Folkers <hello@twobrain.io>
2020-05-28 16:21:25 +0200

    Adding 'abbr' kirbytag

--- build/site/plugins/fv-methods/index.php
+++ build/site/plugins/fv-methods/index.php
@@ -1,6 +1,20 @@
 <?php
 
 Kirby::plugin('fundevogel/methods', [
+    'tags' => [
+        'short' => [
+            'attr' => [
+                'desc',
+                'color',
+            ],
+            'html' => function($tag) {
+                if ($tag->color === 'orange') {
+                    return '<abbr class="js-tippy font-normal text-orange-medium hover:text-orange-dark border-b-2 border-dashed border-orange-medium hover:border-orange-dark cursor-help" title="' . $tag->desc . '" data-tippy-theme="fundevogel orange">' . $tag->value . '</abbr>';
+                }
+                return '<abbr class="js-tippy font-normal text-red-medium hover:text-red-dark border-b-2 border-dashed border-red-medium hover:border-red-dark cursor-help" title="' . $tag->desc . '" data-tippy-theme="fundevogel red">' . $tag->value . '</abbr>';
+            }
+        ]
+    ],
     'fileMethods' => [
         'getCover' => function () {
             $seasons = [
