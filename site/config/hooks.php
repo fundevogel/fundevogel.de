165f7926a5368361dd4ad9ba1133ddbce0f8542f
Martin Folkers <hello@twobrain.io>
2020-06-12 23:34:53 +0200

    Extracting list styles for content-only areas

--- site/config/hooks.php
+++ site/config/hooks.php
@@ -5,6 +5,20 @@
 ##
 
 return [
+    'kirbytext:after' => function ($text) {
+        // See https://forum.getkirby.com/t/add-classes-to-textarea-field-output/14060/5
+        $from = [];
+        $from[0] = '/<p>/';
+        $from[1] = '/<ul>/';
+        $from[2] = '/<ol>/';
+
+        $to = [];
+        $to[0] = '<p class="content">';
+        $to[2] = '<ol class="list">';
+        $to[1] = '<ul class="list">';
+
+        return preg_replace($from, $to, $text);
+    },
     'file.create:after' => function ($file) {
         if ($file->template() == 'pdf') {
             $extension = 'jpg';
