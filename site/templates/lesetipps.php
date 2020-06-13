fe0f138b36e0736b203914cb4675743f2a4be3d0
Martin Folkers <hello@twobrain.io>
2020-06-13 18:09:06 +0200

    Adding download overlay to 'lesetipps'

--- site/templates/lesetipps.php
+++ site/templates/lesetipps.php
@@ -9,12 +9,18 @@
             </div>
             <div class="mt-6 lg:mt-12 flex-none text-center">
                 <?php foreach ($editions as $edition) : ?>
-                <figure class="w-40 sm:w-auto inline-block lg:first:ml-12 last:ml-12 shadow-cover rounded-lg">
-                    <a class="" href="<?= $edition->url() ?>" target="_blank">
+                <a class="inline-block group relative lg:first:ml-12 last:ml-12" href="<?= $edition->url() ?>" target="_blank">
+                    <figure class="w-40 sm:w-auto shadow-cover rounded-lg">
                         <?= $edition->getCover('rounded-t-lg') ?>
                         <figcaption class="py-1 text-sm text-white text-shadow text-center bg-red-medium rounded-b-lg"><?= $edition->edition() ?></figcaption>
-                    </a>
-                </figure>
+                    </figure>
+                    <div class="inset-0 w-full h-full absolute opacity-0 group-hover:opacity-100 rounded-lg bg-red-medium transition-all z-25">
+                        <div class="h-full flex flex-col justify-center items-center">
+                            <?= $site->useSVG('Download', 'w-12 h-12 text-white fill-current', 'download') ?>
+                            <span class="font-normal text-xl text-white">Download</span>
+                        </div>
+                    </div>
+                </a>
                 <?php endforeach ?>
             </div>
         </div>
