00e87bceac2131e1cd43d776a691c1917b447bec
Martin Folkers <hello@twobrain.io>
2020-06-13 21:06:32 +0200

    Adding download overlay to 'lesetipps.archive'

--- site/templates/lesetipps.archive.php
+++ site/templates/lesetipps.archive.php
@@ -17,12 +17,18 @@
         <div class="flex flex-wrap">
             <?php foreach ($editions as $edition) : ?>
             <div class="w-1/2 sm:w-1/3 lg:w-1/4 mb-6 md:mb-10 text-center">
-                <figure class="w-40 md:w-auto inline-block shadow-cover rounded-lg">
-                    <a class="" href="<?= $edition->url() ?>" target="_blank">
+                <a class="inline-block group relative" href="<?= $edition->url() ?>" target="_blank">
+                    <figure class="w-40 md:w-auto inline-block shadow-cover rounded-lg">
                         <?= $edition->getCover('rounded-t-lg') ?>
-                    </a>
-                    <figcaption class="py-1 text-sm text-white text-shadow text-center bg-red-medium rounded-b-lg"><?= implode(' ', [$edition->edition(), $edition->year()]) ?></figcaption>
-                </figure>
+                        <figcaption class="py-1 text-sm text-white text-shadow text-center bg-red-medium rounded-b-lg"><?= implode(' ', [$edition->edition(), $edition->year()]) ?></figcaption>
+                    </figure>
+                    <div class="inset-0 w-full h-full absolute opacity-0 group-hover:opacity-100 rounded-lg bg-red-medium transition-all z-25">
+                        <div class="h-full flex flex-col justify-center items-center">
+                            <?= $site->useSVG('Download', 'w-12 h-12 text-white fill-current', 'download') ?>
+                            <span class="font-normal text-xl text-white">Download</span>
+                        </div>
+                    </div>
+                </a>
             </div>
             <?php endforeach ?>
         </div>
