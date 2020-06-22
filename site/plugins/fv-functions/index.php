143a32961d3906bf0cc66ccef2fa8ec9f977a6fb
Martin Folkers <hello@twobrain.io>
2020-06-22 11:05:00 +0200

    Separating custom functions

---
+++ site/plugins/fv-functions/index.php
@@ -0,0 +1,26 @@
+<?php
+
+function getLangVars ($language = 'de')
+{
+    $translations = Yaml::decode(F::read(
+        kirby()->root('languages') . '/vars/' . $language . '.yml')
+    );
+
+    return $translations;
+}
+
+function useSVG ($title, $classes = '', $file = '', $customAttribute = '')
+{
+    if ($file === '') {
+        $file = str_replace('-', '', $title);
+        $file = strtolower($file);
+    }
+
+    return '<svg class="' . $classes . '" title="' . $title . '" role="img"' . r($customAttribute !== '', ' ' . $customAttribute) . '><use xlink:href="/assets/images/icons.svg#' . $file . '"></use></svg>';
+}
+
+function useSeparator ($color = 'orange-light', $position = 'top') {
+    $margin = Str::contains($position, 'top') === true ? '-mb-px' : '-mt-px';
+
+    return '<div class="w-full"><svg class="w-full h-auto ' . $margin . ' fill-current text-' . $color .'" role="img"><use xlink:href="/assets/images/icons.svg#' . $position . '"></use></svg></div>';
+}
