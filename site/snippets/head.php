fc371b673cf588c854ac8c7613fb0e4d644b9d6f
Martin Folkers <hello@twobrain.io>
2020-07-21 02:31:10 +0200

    Raising Mozilla observatory to grade B

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -1,21 +1,40 @@
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <link rel="preload" href="/assets/fonts/Dosis-Light.subset.woff2" as="font" type="font/woff2" crossorigin>
-  <link rel="preload" href="/assets/fonts/CabinSketch.subset.woff2" as="font" type="font/woff2" crossorigin>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <!-- <meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self'; font-src 'self';"> -->
 
-  <?= $page->metaTags() ?>
+    <!-- Fonts -->
+    <link rel="preload" href="/assets/fonts/Dosis-Light.subset.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="/assets/fonts/CabinSketch.subset.woff2" as="font" type="font/woff2" crossorigin>
 
-  <!-- CSS -->
-  <?php $css = option('debug') === true ? 'main.css' : 'main.min.css'; ?>
-  <style><?= (new Asset('assets/styles/' . $css))->read() ?></style>
-  <noscript><?= css('assets/styles/' . $css) ?></noscript>
+    <!-- CSS -->
+    <?php
+        $cssFile = option('debug') === true ? 'main.css' : 'main.min.css';
+        $cssPath = 'assets/styles/' . $cssFile;
 
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="<?= url('favicon.ico') ?>">
-  <?php snippet('favicons') ?>
+        // echo Bnomei\Fingerprint::css($cssPath, ['integrity' => true])
+    ?>
+    <style><?= (new Asset($cssPath))->read() ?></style>
 
-  <!-- JS -->
-  <?php $js = option('debug') ? 'main.js' : 'main.min.js'; ?>
-  <?= js('assets/scripts/' . $js, ['defer' => true]) ?>
+    <!-- JS -->
+    <?php
+        $jsFile = option('debug') ? 'main.js' : 'main.min.js';
+        $jsPath = 'assets/scripts/' . $jsFile;
+
+        echo js($jsPath, [
+            'defer' => true,
+        ]);
+
+        // echo Bnomei\Fingerprint::js($jsPath, [
+        //     'defer' => true,
+        //     'integrity' => true,
+        // ]);
+    ?>
+
+    <!-- Metadata -->
+    <?= $page->metaTags() ?>
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="<?= url('favicon.ico') ?>">
+    <?php snippet('favicons') ?>
 </head>
