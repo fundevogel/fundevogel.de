117e75e72511fcc967b59bed5e1b2a477fec7306
Martin Folkers <hello@twobrain.io>
2020-09-07 20:45:38 +0200

    Adding script for thumb generation & debugging

---
+++ lib/toolset/check_pagelinks.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'vendor/autoload.php';
+
+# Init Kirby
+$kirby = new Kirby([
+  'roots' => [
+    'base'     => $base = '.',
+    'index'    => $base . '/public',
+    'site'     => $base . '/site',
+    'content'  => $base . '/content',
+    'storage'  => $storage = $base . '/storage',
+    'accounts' => $storage . '/accounts',
+    'cache'    => $storage . '/cache',
+    'sessions' => $storage . '/sessions',
+  ]
+]);
+
+# Loop through all pages
+foreach ($kirby->site()->index() as $page) {
+    try {
+        # Render page
+        echo 'Rendering ' . $page->id() . ' ..';
+        $page->render();
+
+        # Report either `render()` being successful ..
+        echo " done!\n";
+    } catch (\Exception $e) {
+        # .. or output error message upon failure
+        echo " failed!\n";
+        echo $e->getMessage();
+    }
+}
