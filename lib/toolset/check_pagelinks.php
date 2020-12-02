13025959b1f601c749eeb02f515687745a4672bd
Martin Folkers <hello@twobrain.io>
2020-12-02 20:12:47 +0100

    Correcting indent

--- lib/toolset/check_pagelinks.php
+++ lib/toolset/check_pagelinks.php
@@ -4,16 +4,16 @@ include 'vendor/autoload.php';
 
 # Init Kirby
 $kirby = new Kirby([
-  'roots' => [
-    'base'     => $base = '.',
-    'index'    => $base . '/public',
-    'site'     => $base . '/site',
-    'content'  => $base . '/content',
-    'storage'  => $storage = $base . '/storage',
-    'accounts' => $storage . '/accounts',
-    'cache'    => $storage . '/cache',
-    'sessions' => $storage . '/sessions',
-  ]
+    'roots' => [
+        'base'     => $base = '.',
+        'index'    => $base . '/public',
+        'site'     => $base . '/site',
+        'content'  => $base . '/content',
+        'storage'  => $storage = $base . '/storage',
+        'accounts' => $storage . '/accounts',
+        'cache'    => $storage . '/cache',
+        'sessions' => $storage . '/sessions',
+    ],
 ]);
 
 # Loop through all pages
