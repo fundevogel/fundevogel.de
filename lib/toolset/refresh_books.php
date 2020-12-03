39d1f33ed425b7f9b39653a8eb04265a156d672a
Martin Folkers <hello@twobrain.io>
2020-12-03 21:09:57 +0100

    Adding script for manual book refresh

---
+++ lib/toolset/refresh_books.php
@@ -0,0 +1,47 @@
+<?php
+
+include 'vendor/autoload.php';
+
+# Init Kirby
+$kirby = new Kirby([
+    'roots' => [
+        'base'     => $base = '.',
+        'index'    => $base . '/public',
+        'site'     => $base . '/site',
+        'content'  => $base . '/content',
+        'storage'  => $storage = $base . '/storage',
+        'accounts' => $storage . '/accounts',
+        'cache'    => $storage . '/cache',
+        'sessions' => $storage . '/sessions',
+    ],
+]);
+
+# Get `book` pages
+$pages = $kirby->page('buecher')->children()->listed();
+
+# Store unavailable books
+$block = [
+    'ein-haufen-aerger',
+];
+
+# Loop through them
+foreach ($pages as $page) {
+    # Load book data
+    if (in_array($page->uid(), $block)) {
+        continue;
+    }
+
+    echo $page->uid();
+
+    try {
+        $kirby->impersonate('kirby');
+        $page->updateBook(true, ['shop']);
+
+        sleep(5);
+    } catch (\Exception $e) {
+        echo ' .. failed! Moving on .. ' . "\n";
+        continue;
+    }
+
+    echo ' .. done' . "\n";
+}
