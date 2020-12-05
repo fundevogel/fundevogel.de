de5e2820c8564b71d31d7ed549fa1613e7f90fd4
Martin Folkers <hello@twobrain.io>
2020-12-05 14:20:18 +0100

    Moving image snippet back to FV methods plugin

--- site/plugins/fundevogel/index.php
+++ site/plugins/fundevogel/index.php
@@ -7,5 +7,8 @@ require_once __DIR__ . '/functions.php';
 Kirby::plugin('fundevogel/methods', [
     'fileMethods' => require_once __DIR__ . '/file.php',
     'pageMethods' => require_once __DIR__ . '/page.php',
+    'snippets' => [
+        'webPicture' => __DIR__ . '/snippets/webPicture.php',
+    ],
     'tags'        => require_once __DIR__ . '/tags.php',
 ]);
