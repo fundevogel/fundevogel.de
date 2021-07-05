200b75ca110d5ead379f6f5ef67f9cd44fa266aa
Martin Folkers <hello@twobrain.io>
2021-07-05 18:23:29 +0200

    Implement routes

--- site/config/routes.php
+++ site/config/routes.php
@@ -7,8 +7,33 @@
 return [
     [
         'pattern' => 'news/(:any)',
-        'action' => function() {
-            go('/');
+        'action' => function($any) {
+            if ($any == 'json') {
+                $options = [
+                    'title'       => 'Neues aus dem Fundevogel',
+                    'description' => 'Alle Neuigkeiten rund um den Fundevogel, die Kinder- und Jugendbuchhandlung in Freiburg.',
+                    'feedurl'     => site()->url() . '/news/json/',
+                    'link'        => 'news',
+                    'snippet'     => 'feed/json',
+                ];
+
+                return kirby()->collection('news')->limit(12)->feed($options);
+            }
+
+            if ($any == 'rss') {
+                $options = [
+                    'title'       => 'Neues aus dem Fundevogel',
+                    'description' => 'Alle Neuigkeiten rund um den Fundevogel, die Kinder- und Jugendbuchhandlung in Freiburg.',
+                    'url'         => site()->url(),
+                    'feedurl'     => site()->url() . '/news/rss/',
+                    'link'        => 'news',
+                    'snippet'     => 'feed/rss',
+                ];
+
+                return kirby()->collection('news')->limit(12)->feed($options);
+            }
+
+            return go('news/#' . $any);
         }
     ],
     [
@@ -30,6 +55,66 @@ return [
         'pattern' => 'sitemap',
         'action' => function() {
             return go('sitemap.xml', 301);
+        }
+    ],
+    [
+        'pattern' => 'news/json',
+        'method' => 'GET',
+        'action'  => function () {
+            $options = [
+                'title'       => 'Neues aus dem Fundevogel',
+                'description' => 'Alle Neuigkeiten rund um den Fundevogel, die Kinder- und Jugendbuchhandlung in Freiburg.',
+                'feedurl'     => site()->url() . '/news/json/',
+                'link'        => 'news',
+                'snippet'     => 'feed/json',
+            ];
+
+            return kirby()->collection('news')->limit(12)->feed($options);
+        }
+    ],
+    [
+        'pattern' => 'news/rss',
+        'method' => 'GET',
+        'action'  => function () {
+            $options = [
+                'title'       => 'Neues aus dem Fundevogel',
+                'description' => 'Alle Neuigkeiten rund um den Fundevogel, die Kinder- und Jugendbuchhandlung in Freiburg.',
+                'feedurl'     => site()->url() . '/news/rss/',
+                'link'        => 'news',
+                'snippet'     => 'feed/rss',
+            ];
+
+            return kirby()->collection('news')->limit(12)->feed($options);
+        }
+    ],
+    [
+        'pattern' => 'feeds/json',
+        'method' => 'GET',
+        'action'  => function () {
+            $options = [
+                'title'       => 'Unsere Lesetipps',
+                'description' => 'Wir teilen unsere Lesefreuden mit Euch und berichten von Büchern, die uns besonders inspiriert und begeistert haben.',
+                'feedurl'     => site()->url() . '/feeds/json/',
+                'link'        => 'lesetipps',
+                'snippet'     => 'feed/json',
+            ];
+
+            return kirby()->collection('lesetipps')->limit(48)->feed($options);
+        }
+    ],
+    [
+        'pattern' => 'feeds/rss',
+        'method' => 'GET',
+        'action'  => function () {
+            $options = [
+                'title'       => 'Unsere Lesetipps',
+                'description' => 'Wir teilen unsere Lesefreuden mit Euch und berichten von Büchern, die uns besonders inspiriert und begeistert haben.',
+                'feedurl'     => site()->url() . '/feeds/rss/',
+                'link'        => 'lesetipps',
+                'snippet'     => 'feed/rss',
+            ];
+
+            return kirby()->collection('lesetipps')->limit(48)->feed($options);
         }
     ],
 ];
