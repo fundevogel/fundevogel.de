4cf3c4635cf8982f783617f4356641310bdb3a21
Martin Folkers <hello@twobrain.io>
2021-07-05 16:53:12 +0200

    Further progress

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Fonts -->
     <link rel="preload" href="/assets/fonts/CabinSketch.subset.woff2" as="font" type="font/woff2" crossorigin>
