52b6bf64511b75724ad134f5f64c7d443d6da54f
Martin Folkers <hello@twobrain.io>
2021-07-09 11:26:55 +0200

    Rename & move pages around

--- site/collections/assortment.php
+++ site/collections/assortment.php
@@ -4,6 +4,5 @@ return function ($site) {
     return $site->find('unser-sortiment')
                 ->children()
                 ->listed()
-                ->sortBy('title', 'asc')
                 ->onlyTranslated();
 };
