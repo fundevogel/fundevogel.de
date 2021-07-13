218b312f9215613e0c85ed5cff8417ba335367f6
Martin Folkers <hello@twobrain.io>
2021-07-13 11:45:51 +0200

    Make scripts executable from anywhere in Git repository

--- lib/toolset/lines_of_code.bash
+++ lib/toolset/lines_of_code.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Counts lines of code in all tracked files across our Git repository
+# Counts lines of code in all tracked files across current Git repository
+#
 # Note: This script covers only files recognized as 'programming languages'
 # by `github-linguist`, so basically PHP, TypeScript, CSS, Python & Shell
+#
 # See https://gist.github.com/mandiwise/dc53cb9da00856d7cdbb
+
+# Change to Git repository's top-level directory
+cd "$(git rev-parse --show-toplevel)" || exit
 
 # Gotta count 'em all' - except vendor plugins
 count=$(git ls-files \
@@ -17,13 +22,19 @@ count=$(git ls-files \
     | cut -d ' ' -f3
 )
 
-# Count FV plugin separately
-plugin=$(git ls-files site/plugins/fundevogel/*.php \
+# Count FV plugins separately
+fv=$(git ls-files site/plugins/fundevogel/*.php \
     | xargs wc -l \
     | tail -1 \
     | cut -d ' ' -f2
 )
 
-total=$((count + plugin))
+pcbis=$(git ls-files site/plugins/fundevogel/*.php \
+    | xargs wc -l \
+    | tail -1 \
+    | cut -d ' ' -f2
+)
+
+total=$((count + fv + pcbis))
 
 echo "$total"
