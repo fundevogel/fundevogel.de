eef8a7bd78ad981cb54a387de72efda686b52004
Martin Folkers <hello@twobrain.io>
2021-07-15 14:06:30 +0200

    Migrate 'feeds' to dedicated template - cover included

---
+++ site/templates/feeds.php
@@ -0,0 +1,18 @@
+<?php snippet('header') ?>
+
+<header class="container">
+    <div class="flex flex-col lg:flex-row">
+        <div class="flex-1">
+            <?= $page->text()->kt() ?>
+        </div>
+        <div class="mt-12 flex-none text-center">
+            <?php snippet('cover') ?>
+        </div>
+    </div>
+</header>
+<hr>
+<section class="container">
+    <?= $page->details()->kt() ?>
+</section>
+
+<?php snippet('footer') ?>
