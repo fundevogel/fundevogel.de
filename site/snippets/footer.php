02f42a2962cfa1261d323fe4b72352648548dba2
Martin Folkers <hello@twobrain.io>
2021-07-20 16:22:41 +0200

    Add support for 'Mastodon'

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,7 +4,7 @@
             <footer id="site-footer" class="py-8 flex-none relative text-white text-center text-shadow bg-red-medium" role="contentinfo" itemscope itemtype="https://schema.org/WPFooter">
                 <div class="zigzag-border w-full absolute z-10"></div>
                 <div class="container">
-                    <h4 class="mb-4 text-lg sm:text-xl md:text-2xl lg:text-3xl"><?= $site->title()->html() ?></h4>
+                    <h4 class="mb-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">- <?= $site->title()->html() ?> -</h4>
                     <div class="mb-8 text-sm md:text-lg leading-relaxed">
                         <p>Marienstraße 13, 79098 Freiburg</p>
                         <div class="flex justify-center mb-4">
@@ -20,8 +20,8 @@
                     </div>
                 </div>
                 <div class="container">
-                    <div class="text-sm lg:flex justify-between spread-out">
-                        <div class="flex flex-col lg:flex-row mb-4">
+                    <div class="spread-out">
+                        <div class="text-sm flex flex-col lg:flex-row justify-center mb-4">
                             <a class="js-tippy flex justify-center items-center mb-2 sm:mr-3 outline-none" href="mailto:<?= $site->mail()->html() ?>" title="<?= t('Schreibt uns!') ?>">
                                 <?= useSVG('E-Mail', 'w-6 h-6 fill-current', 'envelope') ?>
                                 <span class="px-2"><?= t('Mail schreiben') ?></span>
@@ -40,8 +40,14 @@
                                 <span class="px-2"><?= t('Digitale Geschichten') ?></span>
                             </a>
                             <?php endif ?>
+                            <?php if ($site->mastodon()->isNotEmpty()) : ?>
+                            <a class="js-tippy flex justify-center items-center mb-2 sm:mr-3 outline-none" href="<?= $site->mastodon() ?>" title="<?= t('Mastodon') ?>" target="_blank">
+                                <?= useSVG('Mastodon', 'w-6 h-6 fill-current') ?>
+                                <span class="px-2"><?= t('Gemeinsam dezentral') ?></span>
+                            </a>
+                            <?php endif ?>
                         </div>
-                        <nav class="flex flex-wrap justify-center mb-4" role="navigation">
+                        <nav class="text-xs flex flex-wrap justify-center mb-4" role="navigation">
                             <a class="js-tippy outline-none" href="<?= url('unsere-agb') ?>" title="<?= t('Allgemeine Geschäftsbedingungen') ?>">
                                 <span><?= t('AGB') ?></span>
                             </a>
