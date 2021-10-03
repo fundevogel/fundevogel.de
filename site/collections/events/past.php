4cc6171fb7405339e89cb632e67785440dc6a720
Martin Folkers <hello@twobrain.io>
2021-10-03 13:08:53 +0200

    Fix collection 'events/past'

--- site/collections/events/past.php
+++ site/collections/events/past.php
@@ -1,19 +1,17 @@
 <?php
 
 return function ($kirby) {
-    $events = $kirby->collection('events/all');
-
     # Process events by ..
-    return $events
-        # (1) .. sorting by date
+    return $kirby->collection('events/all')
+        # (1) .. filtering by date
         ->filter(function ($child) {
-            if ($child->multiple_days()->toBool() === true) {
-                return $child->end_date()->toDate() < time();
+            if ($child->dateEnd()->isNotEmpty()) {
+                return $child->dateEnd()->toDate() < time();
             }
 
             return $child->date()->toDate() < time();
 
-        # (2) .. filtering by date
+        # (2) .. sorting by date
         })->sortBy(function ($event) {
             return $event->date()->toDate();
         }, 'desc');
